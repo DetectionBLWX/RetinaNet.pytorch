b11d7d2f4a2f042bd15ad052c51bbe1ce6c387d1
CharlesPikachu <1159254961@qq.com>
2020-03-07 19:05:03 +0800

    Update focalLoss.py

--- modules/losses/focalLoss.py
+++ modules/losses/focalLoss.py
@@ -5,6 +5,7 @@ Author:
 	Charles
 '''
 import torch.nn as nn
+import torch.nn.functional as F
 from libs.sigmoid_focal_loss.sigmoid_focal_loss import sigmoid_focal_loss
 
 
@@ -20,3 +21,14 @@ class FocalLoss(nn.Module):
 		loss = sigmoid_focal_loss(preds, targets, self.gamma, self.alpha)
 		loss = loss.mean() if self.size_average else loss.sum()
 		return self.loss_weight * loss
+
+
+'''for test'''
+def pySigmoidFocalLoss(preds, targets, loss_weight=1.0, gamma=2.0, alpha=0.25, size_average=True):
+	preds_sigmoid = preds.sigmoid()
+	targets = targets.type_as(preds)
+	pt = (1 - preds_sigmoid) * targets + preds_sigmoid * (1 - targets)
+	focal_weight = (alpha * targets + (1 - alpha) * (1 - targets)) * pt.pow(gamma)
+	loss = F.binary_cross_entropy_with_logits(preds, targets, reduction='none') * focal_weight
+	loss = loss.mean() if size_average else loss.sum()
+	return loss * loss_weight
