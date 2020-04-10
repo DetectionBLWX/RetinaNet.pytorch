1b1931b7fe3663867696aeea4aa7ba3a8140bd7a
CharlesPikachu <1159254961@qq.com>
2020-04-10 21:14:30 +0800

    correct fpn according to original paper

--- modules/backbones/FPNResNets.py
+++ modules/backbones/FPNResNets.py
@@ -54,6 +54,7 @@ class FPNResNets(nn.Module):
 		# add smooth layers
 		self.smooth_layer0 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
 		self.smooth_layer1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+		self.smooth_layer2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
 		# add downsample layers
 		self.downsample_layer0 = nn.Conv2d(in_channels=in_channels[0], out_channels=256, kernel_size=3, stride=2, padding=1)
 		self.downsample_layer1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1)
@@ -65,14 +66,16 @@ class FPNResNets(nn.Module):
 		c3 = self.base_layer2(c2)
 		c4 = self.base_layer3(c3)
 		c5 = self.base_layer4(c4)
-		# construct fpn
-		p6 = self.downsample_layer0(c5)
-		p7 = self.downsample_layer1(F.relu(p6, inplace=True))
+		# top-down path
 		p5 = self.lateral_layer0(c5)
 		p4 = self.upsampleAdd(p5, self.lateral_layer1(c4))
-		p4 = self.smooth_layer0(p4)
 		p3 = self.upsampleAdd(p4, self.lateral_layer2(c3))
+		# obtain fpn features
+		p5 = self.smooth_layer0(p5)
+		p4 = self.smooth_layer1(p4)
 		p3 = self.smooth_layer1(p3)
+		p6 = self.downsample_layer0(c5)
+		p7 = self.downsample_layer1(F.relu(p6, inplace=True))
 		# return all feature pyramid levels
 		return [p3, p4, p5, p6, p7]
 	'''upsample and add'''
@@ -90,15 +93,21 @@ class FPNResNets(nn.Module):
 	def initializeAddedLayers(self, init_method='xavier'):
 		# normal init
 		if init_method == 'normal':
-			for layer in [self.lateral_layer0, self.lateral_layer1, self.lateral_layer2, self.smooth_layer0, self.smooth_layer1, self.downsample_layer0, self.downsample_layer1]:
+			for layer in [self.lateral_layer0, self.lateral_layer1, self.lateral_layer2, 
+						  self.smooth_layer0, self.smooth_layer1, self.smooth_layer2,
+						  self.downsample_layer0, self.downsample_layer1]:
 				normalInit(layer, std=0.01)
 		# kaiming init
 		elif init_method == 'kaiming':
-			for layer in [self.lateral_layer0, self.lateral_layer1, self.lateral_layer2, self.smooth_layer0, self.smooth_layer1, self.downsample_layer0, self.downsample_layer1]:
+			for layer in [self.lateral_layer0, self.lateral_layer1, self.lateral_layer2, 
+						  self.smooth_layer0, self.smooth_layer1, self.smooth_layer2,
+						  self.downsample_layer0, self.downsample_layer1]:
 				kaimingInit(layer, nonlinearity='relu')
 		# xavier init
 		elif init_method == 'xavier':
-			for layer in [self.lateral_layer0, self.lateral_layer1, self.lateral_layer2, self.smooth_layer0, self.smooth_layer1, self.downsample_layer0, self.downsample_layer1]:
+			for layer in [self.lateral_layer0, self.lateral_layer1, self.lateral_layer2, 
+						  self.smooth_layer0, self.smooth_layer1, self.smooth_layer2,
+						  self.downsample_layer0, self.downsample_layer1]:
 				xavierInit(layer, distribution='uniform')
 		# unsupport
 		else:
